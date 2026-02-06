265306e381830957079652ae64ddafc7d609e588
Neil Burrows <neil@pawprint.co.uk>
2026-02-06 15:44:31 +0000

    switch base to bluefin-dx-nvidia-open & add in Hyperland

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,6 +4,7 @@ set -ouex pipefail
 
 ### Install packages
 
+rpm-ostree install waybar fontawesome-fonts-all
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
@@ -19,6 +20,9 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+dnf5 -y copr enable solopasha/hyprland
+dnf5 -y install xdg-desktop-portal-hyprland hyprland cliphist eww-git hypridle hyprlock hyprshot waypaper
+dnf5 -y copr disable solopasha/hyprland
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
